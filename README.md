417fd2ae22f989cda844f8442bbdda2559d70dd7
Gautam Sagar <gautamsagar.mallela99@gmail.com>
2022-10-26 21:15:28 -0500

    commited with bonus

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 New Express App using Pug with index and user pages.
 
-Added a line for testing local server with remote server.
+link to my remote server: <https://f2wa19mallelagautam.onrender.com>
--- routes/computation.js
+++ routes/computation.js
@@ -4,8 +4,16 @@ var router = express.Router();
 /* Compuation */
 router.get('/', function(req, res, next) {
     let randomValue = Math.round(Math.random()*10);
-    let sqrtValue = Math.round(Math.sqrt(randomValue));
-  res.send(`Math.sqrt()  applied to ${randomValue} is ${sqrtValue}`);
+    let absValue = Math.round(Math.abs(randomValue));
+    let acosValue = Math.acos(randomValue);
+    let sinValue = Math.round(Math.sin(randomValue));
+    let sinhValue = Math.round(Math.sinh(randomValue));
+  res.send(`Math.abs()  applied to ${randomValue} is ${absValue} <br> 
+  Math.acos()  applied to ${randomValue} is ${acosValue} <br>
+  Math.sin()  applied to ${randomValue} is ${sinValue} <br>
+  Math.sinh()  applied to ${randomValue} is ${sinhValue} <br>
+  
+  `);
 });
 
 module.exports = router;
